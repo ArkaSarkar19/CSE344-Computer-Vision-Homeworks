cec33da042b0cd2698724455ea5b65add6918d6a
ArkaSarkar19 <arka18222@iiitd.ac.in>
2021-01-20 21:34:49 +0530

    Homework 2 added

--- HW2/2018222_HW2.py
+++ HW2/2018222_HW2.py
@@ -1,117 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import cv2
 import numpy as np 
 import matplotlib.pyplot as plt
 import copy
 
 
-def threshold(image):
-
-	R = image[:,:,0]
-	G = image[:,:,1]
-	B = image[:,:,2]
-
-
-	# cv2.imshow('R',image)
-	# cv2.waitKey(0)
-	# cv2.destroyAllWindows()
-
-	# cv2.imshow('G',G)
-	# cv2.waitKey(0)
-	# cv2.destroyAllWindows()
-
-
-	# cv2.imshow('B',B)
-	# cv2.waitKey(0)
-	# cv2.destroyAllWindows()
-
-	threshold_R = otsu(R)
-	threshold_G = otsu(G)
-	threshold_B = otsu(B)
-
-	threshold = (threshold_R + threshold_G + threshold_B )>2
-
-	cv2.imshow('thres_R',threshold_R*R)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-	cv2.imshow('thres_G',threshold_G*G)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-
-	cv2.imshow('thres_B',threshold_B*B)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-	threshold = np.dstack((threshold,threshold,threshold))
-
-	cv2.imshow('thres',1- threshold*np.ones(threshold.shape))
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-
-
-	return image*threshold
+# In[2]:
 
 
 
 def otsu(img):
 
-	min_cost = float('inf')
-	threshold = 0
+    min_cost = float('inf')
+    threshold = 0
 
-	for i in range(1,256):
-		v0 = np.var(img[img < i], ddof = 1)
-		w0 = len(img[img < i])
-		v1 = np.var(img[img >= i], ddof = 1)
-		w1 = len(img[img >= i])
+    for i in range(1,256):
+        v0 = np.var(img[img < i], ddof = 1)
+        w0 = len(img[img < i])
+        v1 = np.var(img[img >= i], ddof = 1)
+        w1 = len(img[img >= i])
 
-		cost = w0*v0 + w1*v1
-		if(cost < min_cost):
-			min_cost = cost
-			threshold = i
+        cost = w0*v0 + w1*v1
+        if(cost < min_cost):
+            min_cost = cost
+            threshold = i
 
-	return threshold
-
-
+    return threshold
 
 
-if __name__ == "__main__":
-
-	# img = cv2.imread("dog-1020790_960_720.jpg", 2)
-	# img.astype(int)
+# In[3]:
 
 
+def select_foreground(img, s = 0):
+    if(s == 0):
+        m,n = img.shape
+        
+        m0 = int(0.2*m)
+        n0 = int(0.2*n)
+        c0 = 0
+        c1 = 0
+        for i in range(m//2 - m0, m//2 + m0):
+            for j in range(n//2 - n0, n//2 + n0):
+                if(img[i,j] == 0):
+                    c0 = c0 + 1
+                else:
+                    c1 = c1 + 1
+        if(c0 > c1):
+            return 0
+        else :
+            return 1
+    else : 
+        
+        m,n = img.shape
+        
+        m0 = 10
+        n0 = 10
+        c0 = 0
+        c1 = 0
+        for i in range(m):
+            for j in range(n):
+                if (i < m0): 
+                    if(img[i,j] == 0):
+                        c0 = c0 + 1
+                    else:
+                        c1 = c1 + 1
+                    
+                elif (i > m-m0): 
+                    if(img[i,j] == 0):
+                        c0 = c0 + 1
+                    else:
+                        c1 = c1 + 1
+                elif (j < n0): 
+                    if(img[i,j] == 0):
+                        c0 = c0 + 1
+                    else:
+                        c1 = c1 + 1
+                elif (j > n-n0): 
+                    if(img[i,j] == 0):
+                        c0 = c0 + 1
+                    else:
+                        c1 = c1 + 1
+                
+        if(c0 > c1):
+            return 0
+        else :
+            return 1
+        
+        
 
-	# thres = otsu(img)
+
+# In[6]:
 
 
-	# img[img<thres] = 777
-		
-	# cv2.imshow('image',img)
-	# cv2.waitKey(0)
-	# cv2.destroyAllWindows()
+img = cv2.imread("dog-1020790_960_720.jpg", 0)
 
-	# image = cv2.imread("dog-1020790_960_720.jpg")
-	# image.astype(int)
-	# image[:,:,0][img==777] = 255
-	# image[:,:,1][img==777] = 0
-	# image[:,:,2][img==777] = 0
+plt.figure(figsize=(10,10))
+plt.imshow(img)
+plt.title("Real Image Grayscale")
+plt.show()
+
+img.astype(int)
+thres = otsu(img)
+img[img<thres] = 0
+img[img >= thres] = 1
+fore = select_foreground(img,0)
+plt.figure(figsize=(10,10))
+plt.imshow(img)
+plt.title("Threshold")
+plt.show()
+
+print("Otsu Threshold : ",thres)
+img2 = cv2.imread("dog-1020790_960_720.jpg")
+img = cv2.imread("dog-1020790_960_720.jpg", 2)
+if(fore == 1):
+    img[img <= thres]= -1
+else :
+    img[img >= thres]= -1
+
+    
+B = img2[:,:,0]
+G = img2[:,:,1]
+R = img2[:,:,2]
+
+m,n = R.shape
+for i in range(m):
+    for j in range(n):
+        if(img[i,j] == 255):
+            B[i,j] = 255
+            R[i,j] = 0
+            G[i,j] = 0
+final_image =  np.dstack((R,G,B))
+plt.figure(figsize=(10,10))
+plt.imshow(final_image)
+plt.title("Otsu Image")
+plt.show()
 
 
 
-	# cv2.imshow('otsu',image)
-	# cv2.waitKey(0)
-	# cv2.destroyAllWindows()
-
-	# print(otsu.shape)
+# In[ ]:
 
 
-	image = cv2.imread("dog-1020790_960_720.jpg")
-	image.astype(int)
 
-	i = threshold(image)
 
-	cv2.imshow('otsu',i)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
